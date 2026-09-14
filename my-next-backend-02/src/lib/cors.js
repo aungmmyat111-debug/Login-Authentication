c84e55010ab8afd93053e79570bfd5a7ab7d4372
aungmmyat111-debug <aungmmyat111@gmail.com>
2026-09-14 14:56:47 +0700

    fixed cors

--- my-next-backend-02/src/lib/cors.js
+++ my-next-backend-02/src/lib/cors.js
@@ -1,7 +1,7 @@
 // src/lib/cors.js
 const corsHeaders = {
   "Access-Control-Allow-Credentials": "true",
-  "Access-Control-Allow-Origin": process.env.FRONTEND_URL,
+  "Access-Control-Allow-Origin": "https://login-authentication-chi-nine.vercel.app",
   "Access-Control-Allow-Methods": "GET, POST, PUT, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
   "Access-Control-Max-Age": "86400",
